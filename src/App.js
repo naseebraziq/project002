8792cdb980132460c2c09c1d693e0f39e3ebf42f
naseebraziq <61156643+naseebraziq@users.noreply.github.com>
2020-06-23 15:30:54 -0700

    deploy on surge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome All <code>src/App.js</code> welcome Dear naseeb.
+          Finally deploye on surge <code>src/App.js</code> welcome Dear naseeb.
         </p>
         <a
           className="App-link"
